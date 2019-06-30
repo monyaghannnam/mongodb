4cb01532709549e76622e2c87a3f9f83749e0b4c
monyaghannam <monya.jamal.96@gmail.com>
2019-06-30 19:39:48 +0300

    second

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-from pymongo import MongoClient
+from mongoengine import *
 
-client = MongoClient()
+connect('db',host='localhost',port=27017)
 
+class post(Document):
+    name=StringField()
+    published=BooleanField()
+
+    @queryset_manager
+    def retrive(self,queryset):
+        return queryset.filter(published=True)
+post1 =post(name="monya",published=True)
+post1.save()
+post1.name='meme'
+post1.save()
+print post.retrive()
